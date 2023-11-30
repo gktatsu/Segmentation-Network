2ee015c68f9dd72cfdca6b8dbed6735fab4fc319
1p4ssi1 <pascal.rapp@uni-ulm.de>
2023-11-29 16:36:41 -0800

    change dataset to big augmented dataset

--- Network/pyimagesearch/config.py
+++ Network/pyimagesearch/config.py
@@ -12,12 +12,12 @@ import os
 # DATASET_PATH": r"C:/Users/Platz3/PycharmProjects/Segmentation-Network/Network/Dataset/AugDataset/WannerFIB/"
 
 config_dic = {
-    "DATASET_PATH": r"Network/Dataset/AugDataset/WannerFIB/",
+    "DATASET_PATH": r"Network/Dataset/AugDatasetBig/WannerFIB/",
     "DEVICE": "cuda" if torch.cuda.is_available() else "cpu",
     "PIN_MEMORY": True if "cuda" else False,
     "NUM_CLASSES": 3,
     "INIT_LR": 0.001,
-    "NUM_EPOCHS": 500,
+    "NUM_EPOCHS": 600,
     "BATCH_SIZE": 64,
     "INPUT_IMAGE_WIDTH": 128,
     "INPUT_IMAGE_HEIGHT": 128,
