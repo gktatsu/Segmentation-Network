7ab68f56b555df094789b94c6bf87867a18d82aa
Pascal <pascal.rapp@uni-ulm.de>
2023-12-19 15:57:50 +0100

    Change Dataset

--- Network/pyimagesearch/config.py
+++ Network/pyimagesearch/config.py
@@ -16,12 +16,12 @@ import os
 
 # "BASE_OUTPUT": "output",
 config_dic = {
-    "DATASET_PATH": r"Network/Dataset/CenterCrop/",
+    "DATASET_PATH": r"Network/Dataset/RotationCropNoise/",
     "DEVICE": "cuda" if torch.cuda.is_available() else "cpu",
     "PIN_MEMORY": True if "cuda" else False,
     "NUM_CLASSES": 3,
     "NUM_WORKERS": 0,
-    "INIT_LR": 0.001,
+    "INIT_LR": 0.0007,
     "NUM_EPOCHS": 400,
     "BATCH_SIZE": 512,
     "INPUT_IMAGE_WIDTH": 128,
