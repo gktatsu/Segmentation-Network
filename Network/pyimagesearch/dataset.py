fcc154eb2a0161ca0cfe335b8579d98a8b8325be
Pascal <pascal.rapp@uni-ulm.de>
2023-12-26 14:22:33 +0100

    Probability for real and augmented images

--- Network/pyimagesearch/dataset.py
+++ Network/pyimagesearch/dataset.py
@@ -6,6 +6,7 @@ import cv2
 import glob
 import torch
 import matplotlib.pyplot as plt
+import random
 
 class SegmentationDataset(Dataset):
 	def __init__(self, imagePaths, maskPaths, transforms):
@@ -20,40 +21,34 @@ class SegmentationDataset(Dataset):
 		# return the number of total samples contained in the dataset
 		return len(self.imagePaths)
 	def __getitem__(self, idx):
-		# grab the image path from the current index
-		imagePath = self.imagePaths[idx]
-		# load the image from disk, swap its channels from BGR to RGB,
-		# and read the associated mask from disk in grayscale mode
-		image = cv2.imread(imagePath)
-		"""import pdb
-		pdb.set_trace()"""
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-		mask = cv2.imread(self.maskPaths[idx], 0)
+		probability = random.random() < 0.6
 
+		if probability:
+			# use original image
+			imagePath = self.imagePaths[idx]
+			image = cv2.imread(imagePath)
+			image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+			mask = cv2.imread(self.maskPaths[idx], 0)
+		else:
+			# use augmented image
+			base_image_name = self.imagePaths[idx].split("_aug")[0]
+			augmentations = ["blur_0", "brightness_0", "crop_0", "noise_0", "zoom_rotate_0"]
+			chosen_augmentation = random.choice(augmentations)
+			augmented_image_path = f"{base_image_name}_aug_{chosen_augmentation}.png"
+
+			image = cv2.imread(augmented_image_path)
+			image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+			mask = cv2.imread(self.maskPaths[idx], 0)
 
 		binary_mask = np.zeros((mask.shape[0], mask.shape[1], config.config_dic["NUM_CLASSES"]), dtype=np.uint8)
 
 		for class_idx in range(config.config_dic["NUM_CLASSES"]):
-			binary_mask[:,:,class_idx] = (mask==class_idx).astype(np.uint8)
+			binary_mask[:, :, class_idx] = (mask == class_idx).astype(np.uint8)
 
-		""""""
-		# check to see if we are applying any transformations
-
+		# apply the transformations to both image and its mask
 		if self.transforms is not None:
-			# apply the transformations to both image and its mask
 			image = self.transforms(image)
 			mask = self.transforms(binary_mask)
-			mask = (mask*255) #.type(torch.IntTensor)
-		"""fig, axs = plt.subplots(1,config.NUM_CLASSES)
-		for i in range(config.NUM_CLASSES):
-			axs[i].imshow(mask[i])
-		plt.show()"""
+			mask = (mask * 255)
 
-		#import pdb
-		#pdb.set_trace
-
-
-			# image.shape sollte sein: C,B,H
-			# mask.shape num_classes,B,H  oder B,H
-		# return a tuple of the image and its mask
 		return (image, mask)
