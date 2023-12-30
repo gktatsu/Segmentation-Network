70ab0f95b6b300f76bdd8d3898fc4038caf81b65
Pascal <pascal.rapp@uni-ulm.de>
2023-12-30 14:33:47 +0100

    lr

--- Network/pyimagesearch/config.py
+++ Network/pyimagesearch/config.py
@@ -16,7 +16,7 @@ import os
 
 # "BASE_OUTPUT": "output",
 config_dic = {
-    "DATASET_PATH": r"Network/Dataset/Random/",
+    "DATASET_PATH": r"Network/Dataset/ContrastSaettigung/",
     "DEVICE": "cuda" if torch.cuda.is_available() else "cpu",
     "PIN_MEMORY": True if "cuda" else False,
     "NUM_CLASSES": 3,
