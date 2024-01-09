2ef394ac13ee33c3331dfec92479c5ff43e22d8a
Pascal <pascal.rapp@uni-ulm.de>
2024-01-09 18:05:30 +0100

    Batchsize and np.max

--- Network/pyimagesearch/config.py
+++ Network/pyimagesearch/config.py
@@ -2,7 +2,7 @@
 import torch
 import os
 
-# Epochs: 40, Patience: 5, Min_num_epochs: 20, INIT_LR: 0.001, Augmentation (XXL_mix) LR: 0.002875
+# Epochs: 40, Patience: 5, Min_num_epochs: 20, INIT_LR: 0.001, Augmentation (XXL_mix) LR: 0.002875, Batch_size: 512
 # "DEVICE": "cuda" if torch.cuda.is_available() else "cpu",
 
 # Path for original dataset
@@ -23,7 +23,7 @@ config_dic = {
     "NUM_WORKERS": 0,
     "INIT_LR": 0.001,
     "NUM_EPOCHS": 400,
-    "BATCH_SIZE": 512,
+    "BATCH_SIZE": 64,
     "INPUT_IMAGE_WIDTH": 128,
     "INPUT_IMAGE_HEIGHT": 128,
     "THRESHOLD": 0.5,
