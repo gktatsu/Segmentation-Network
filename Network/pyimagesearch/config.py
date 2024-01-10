96d84979bd21af3e1c34231c8769fed4538dca77
Pascal <pascal.rapp@uni-ulm.de>
2024-01-10 14:48:20 +0100

    other dataset

--- Network/pyimagesearch/config.py
+++ Network/pyimagesearch/config.py
@@ -16,7 +16,7 @@ import os
 
 # "BASE_OUTPUT": "output",
 config_dic = {
-    "DATASET_PATH": r"Network/Dataset/XXL_mix/",
+    "DATASET_PATH": r"Network/Dataset/Dataset 1/WannerFIB/",
     "DEVICE": "cuda" if torch.cuda.is_available() else "cpu",
     "PIN_MEMORY": True if "cuda" else False,
     "NUM_CLASSES": 3,
