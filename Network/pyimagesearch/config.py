fc844950b274d94df884998f222508b8ca9f569c
Pascal <pascal.rapp@uni-ulm.de>
2024-05-24 13:27:56 +0200

    other dataset

--- Network/pyimagesearch/config.py
+++ Network/pyimagesearch/config.py
@@ -16,7 +16,7 @@ import os
 
 # "BASE_OUTPUT": "output",
 config_dic = {
-    "DATASET_PATH": r"Network/Dataset/Dataset 1 + 232 Synthetic/",
+    "DATASET_PATH": r"Network/Dataset/Real + Augmentations + Synthetic/",
     "DEVICE": "cuda" if torch.cuda.is_available() else "cpu",
     "PIN_MEMORY": True if "cuda" else False,
     "NUM_CLASSES": 3,
