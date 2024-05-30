0d353d73cbda185ff3017433c20a9ca11d870da3
Pascal <pascal.rapp@uni-ulm.de>
2024-05-30 09:51:18 +0200

    other dataset

--- Network/pyimagesearch/config.py
+++ Network/pyimagesearch/config.py
@@ -21,7 +21,7 @@ config_dic = {
     "PIN_MEMORY": True if "cuda" else False,
     "NUM_CLASSES": 3,
     "NUM_WORKERS": 0,
-    "INIT_LR": 0.0001,
+    "INIT_LR": 0.001,
     "NUM_EPOCHS": 400,
     "BATCH_SIZE": 128,
     "INPUT_IMAGE_WIDTH": 128,
