b9a6715c9b14acfb3a0e9f62a315203b39dddf4f
tatsuki_ulm <gktatsu@gmail.com>
2025-10-27 19:36:13 +0100

    debug config.py
    add whitespace

--- Network/pyimagesearch/config.py
+++ Network/pyimagesearch/config.py
@@ -35,7 +35,7 @@ config_dic = {
     "INPUT_IMAGE_HEIGHT": 256,
     "THRESHOLD": 0.5,
     "BASE_OUTPUT": "/mnt/hdd/tatsuki/tatsuki/programs/Segmentation-Network/Network/output/weights/",
-    "PATIENCE":100,
+    "PATIENCE": 100,
     "MIN_NUM_EPOCHS": 100,
     "NUM_LOG_IMAGES": 5,
     "SCHEDULER_FACTOR": 0.1
